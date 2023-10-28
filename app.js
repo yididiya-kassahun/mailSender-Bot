6582262e02bffabc6609ebeeef70a1305e77f26e
yididiya <57259174+yididiya-kassahun@users.noreply.github.com>
2023-10-28 17:16:33 +0300

    mail sender bot done

---
+++ app.js
@@ -0,0 +1,141 @@
+const nodemailer = require('nodemailer');
+const TelegramBot = require("node-telegram-bot-api");
+const TOKEN =
+  process.env.TELEGRAM_TOKEN ||
+  "6706961711:AAFwrG9h27qkYT65iVe8jJlTthrK-j6RBrU";
+
+
+const transporter = nodemailer.createTransport({
+        service: "gmail",
+        port: 465,
+        secure: true,
+        auth: {
+          user: "yididiya.tech@gmail.com",
+          pass: "cxjx hwvz axuh uvqq", 
+        },
+    //   auth: { pjby qlox pmyv qfyq
+    //     api_key:
+    //       "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",
+    //   },
+    });
+
+// ************* Bot
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+// Define a conversation state object to store user inputs
+const conversationState = {};
+
+function startMenu(msg) {
+    const opts = {
+      reply_markup: JSON.stringify({
+        keyboard: [
+          [{ text: "✉️ send mail " }, { text: "👨🏽‍💻 developer" }],
+          [{ text: "Back" }],
+        ],
+        resize_keyboard: true,
+        one_time_keyboard: true,
+      }),
+    };
+    bot.sendMessage(msg.chat.id, " start menu ", opts);
+  }
+  
+  bot.onText(/start/, (msg) => {
+    startMenu(msg);
+  });
+
+bot.onText(/send mail/, (msg) => {
+    startConversation(msg);
+});
+
+function startConversation(msg) {
+    // Initialize the conversation state for the user
+    conversationState[msg.chat.id] = {
+        step: 1, // Current step in the conversation
+        receiverMail: "",
+        subject: "",
+        body: "",
+    };
+
+    askReceiverMail(msg.chat.id);
+}
+
+function askReceiverMail(chatId) {
+    bot.sendMessage(chatId, "Enter the receiver mail:", {
+        reply_markup: { force_reply: true },
+    });
+}
+
+function askSubject(chatId) {
+    bot.sendMessage(chatId, "Enter the mail subject:", {
+        reply_markup: { force_reply: true },
+    });
+}
+
+function askBody(chatId) {
+    bot.sendMessage(chatId, "Enter the mail body:", {
+        reply_markup: { force_reply: true },
+    });
+}
+
+// Handle user replies
+bot.on('message', (msg) => {
+    const chatId = msg.chat.id;
+    const userState = conversationState[chatId];
+
+    if (userState) {
+        switch (userState.step) {
+            case 1:
+                userState.receiverMail = msg.text;
+                userState.step++;
+                askSubject(chatId);
+                break;
+            case 2:
+                userState.subject = msg.text;
+                userState.step++;
+                askBody(chatId);
+                break;
+            case 3:
+                userState.body = msg.text;
+
+                // You now have all the user inputs in userState
+                const { receiverMail, subject, body } = userState;
+                // Do something with the inputs, e.g., send the email
+               const mailResult = sendMail(receiverMail,subject,body);
+               bot.sendMessage(chatId,'Loading ... ');
+
+               setTimeout(()=>{
+                   bot.sendMessage(chatId, mailResult);
+               },4000);
+
+                // Clear the conversation state
+                delete conversationState[chatId];
+
+                break;
+        }
+    }
+   
+});
+
+function sendMail(receiverMail,subject,body){
+   // console.log(receiverMail,subject,body);  
+    try{
+        transporter.sendMail({
+            to: receiverMail,
+            from: "yididiya.tech@gmail.com",
+            subject: subject,
+            text: body
+          });
+         return "Email sent successfuly";
+   
+    }catch(err){
+        throw err;
+    }
+}
+
+bot.onText(/developer/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Developed By: Yididiya Kassahun\n\n ");
+  });
+  
+  bot.onText(/back/, (msg) => {
+    startMenu(msg);
+  });
